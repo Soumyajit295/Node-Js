74671001da175428810815dcb2e2b3ed87f80da4
Soumyajit de <soumyajit.de20190@gmail.com>
2025-02-02 18:57:48 +0530

    Add the basic project

---
+++ 01_hello/index.js
@@ -0,0 +1,9 @@
+console.log("Hello from Node js")
+
+/* What is Node Js : Node js is runtime enviroment which allow us to run javascript outside in browser */
+
+/* console.log(window)  Throws an error window is not defined */
+
+/* Reason behind the error : The window is part of browser it provides DOM,alert and localstorage like functionality but Nodejs is server side runtime and there is no DOM or browser window. Instead of window Nodejs has it's own gloabal object called global */
+
+/* What is the package.json file : The package.json file is the heart of any Node project. It contains the metadata related to the project like the dependecies scripts and configuration of the project */
