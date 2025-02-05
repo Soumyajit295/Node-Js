41ef62fde051ab9872f88500bc5f37a2ba0f5ca8
Soumyajit de <soumyajit.de20190@gmail.com>
2025-02-05 12:07:02 +0530

    Changes in learn_express index.js file

--- learn_express/index.js
+++ learn_express/index.js
@@ -13,3 +13,25 @@ app.get('/about',(req,res)=>{
 app.listen(8000,()=>{
     console.log(`Server is listening at port 8000`)
 })
+
+/* How we create a server traditional way */
+
+// const http = require('http')
+
+// const server = http.createServer((req,res)=>{
+//     switch(req.url){
+//         case '/':
+//             res.end("Hello From Home Page")
+//             break;
+//         case '/about':
+//             res.end("Hello From About Page")
+//             break;
+//         default:
+//             res.end('404 Page Not Found')
+//             break
+//     }
+// })
+
+// server.listen(8000,()=>{
+//     console.log("Server is listening at port 8000")
+// })
