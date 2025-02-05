5222a0bac334da982f7b09b0284458a38c5ab5d9
Soumyajit de <soumyajit.de20190@gmail.com>
2025-02-05 11:59:57 +0530

    Learn how to create a server using Express

---
+++ learn_express/index.js
@@ -0,0 +1,15 @@
+const express = require('express')
+
+const app = express()
+
+app.get('/',(req,res)=>{
+    res.send("Hello From Home Page!")
+})
+
+app.get('/about',(req,res)=>{
+    res.send("Hello From About Page!")
+})
+
+app.listen(8000,()=>{
+    console.log(`Server is listening at port 8000`)
+})
